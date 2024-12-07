20aef4970c259c7df32ca9a429cfda88428206ce
Landon Kleinbrodt <lkleinbrodt@gmail.com>
2024-12-07 15:15:41 -0800

    idk

--- backend/__init__.py
+++ backend/__init__.py
@@ -18,6 +18,18 @@ CORS(
 jwt.init_app(app)
 
 from flask import Blueprint
+from flask import send_from_directory
+import os
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + "/" + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, "index.html")
+
 
 from .twenty_questions.routes import twenty_questions
 
